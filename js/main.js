ea3382d999633108fba7618a55b590ca749d453d
Oleg Gor <147436932+Oleg-78-blip@users.noreply.github.com>
2024-03-06 11:24:52 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const menuBtn = document.querySelector('.menu-btn');
+const menu = document.querySelector('.menu');
+
+menuBtn.addEventListener('click', function() {
+    menu.classList.toggle('active');
+    menuBtn.classList.toggle('active');
+
+})
+
+console.log('OLEG GOR!!!')
+
